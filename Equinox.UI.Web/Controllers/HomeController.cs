260a5cfed7189025c221e71a73b94f2fc1541b82
Gonzalo Mejía <gonzmejiasan@hotmail.com>
2022-04-24 09:23:03 -0500

    Initial commit
    Adding initial code

---
+++ Equinox.UI.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Equinox.UI.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
